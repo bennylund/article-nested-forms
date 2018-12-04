503a0ff820e335b7c0f291b41fd556572bb88dde
bennylund <lund.benny@gmail.com>
2018-12-04 23:59:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# README
+
+from article:
+https://medium.com/@mindovermiles262/triple-nested-forms-in-rails-dedbcccb5799
---
+++ app/models/season.rb
@@ -0,0 +1,5 @@
+class Season < ApplicationRecord
+  belongs_to :show, optional: true
+  has_many :episodes
+  accepts_nested_attributes_for :episodes
+end
